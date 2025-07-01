ff1a1b57bf01aca5b615f709cd5f87b393f480bc
JimFuller-RedHat <jfuller@redhat.com>
2025-07-01 14:05:32 +0200

    enhance: add analysis graph tests

--- src/restapi.rs
+++ src/restapi.rs
@@ -146,3 +146,15 @@ pub async fn get_sbom_license_ids(sbom_id: String, user: &mut GooseUser) -> Tran
 
     Ok(())
 }
+
+pub async fn get_analysis_status(user: &mut GooseUser) -> TransactionResult {
+    let _response = user.get("/api/v2/analysis/status").await?;
+
+    Ok(())
+}
+
+pub async fn get_analysis_latest_cpe(user: &mut GooseUser) -> TransactionResult {
+    let _response = user.get("/api/v2/analysis/latest/component/cpe%3A%2Fa%3Aredhat%3Aopenshift_builds%3A1.3%3A%3Ael9").await?;
+
+    Ok(())
+}
